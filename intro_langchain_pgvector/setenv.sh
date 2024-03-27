9fd1a77d858ccaa82f5f43861ebd2231efbd6c6f
richardsacosta <115644651+richardsacosta@users.noreply.github.com>
2024-03-27 16:54:07 -0500

    Vector Cookbook project PostgreSQL and  pgVector

---
+++ intro_langchain_pgvector/setenv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export OPENAI_API_KEY=sk-********************************
+export TIMESCALE_HOST=YOUR_DATABASE_CONNECTION_INFO
+export TIMESCALE_PORT=
+export TIMESCALE_USER=
+export TIMESCALE_PASSWORD=
+export TIMESCALE_DBNAME=
